bb0757ba1d373db8e33484048c220621e9033be5
nitesh-dev <niteshdev547@gmail.com>
2023-06-09 21:27:02 +0530

    dashboard page 30% completed

---
+++ front/components/Sidebar.vue
@@ -0,0 +1,120 @@
+<script setup lang='ts'>
+</script>
+<template>
+    <aside>
+        <h2>Flax Studio</h2>
+        <hr>
+        <ul>
+            <li class="active">
+                <span class="marker"></span>
+                <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M10.55 2.532a2.25 2.25 0 0 1 2.9 0l6.75 5.692c.507.428.8 1.057.8 1.72v9.803a1.75 1.75 0 0 1-1.75 1.75h-3.5a1.75 1.75 0 0 1-1.75-1.75v-5.5a.25.25 0 0 0-.25-.25h-3.5a.25.25 0 0 0-.25.25v5.5a1.75 1.75 0 0 1-1.75 1.75h-3.5A1.75 1.75 0 0 1 3 19.747V9.944c0-.663.293-1.292.8-1.72l6.75-5.692Zm1.933 1.147a.75.75 0 0 0-.966 0L4.767 9.37a.75.75 0 0 0-.267.573v9.803c0 .138.112.25.25.25h3.5a.25.25 0 0 0 .25-.25v-5.5c0-.967.784-1.75 1.75-1.75h3.5c.966 0 1.75.783 1.75 1.75v5.5c0 .138.112.25.25.25h3.5a.25.25 0 0 0 .25-.25V9.944a.75.75 0 0 0-.267-.573l-6.75-5.692Z" />
+                </svg>
+                <span>Home</span>
+            </li>
+            <li>
+                <span class="marker"></span>
+                <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="m18.492 2.33 3.179 3.179a2.25 2.25 0 0 1 0 3.182l-2.584 2.584A2.25 2.25 0 0 1 21 13.5v5.25A2.25 2.25 0 0 1 18.75 21H5.25A2.25 2.25 0 0 1 3 18.75V5.25A2.25 2.25 0 0 1 5.25 3h5.25a2.25 2.25 0 0 1 2.225 1.915L15.31 2.33a2.25 2.25 0 0 1 3.182 0ZM4.5 18.75c0 .414.336.75.75.75l5.999-.001.001-6.75H4.5v6Zm8.249.749h6.001a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75h-6.001v6.75Zm-2.249-15H5.25a.75.75 0 0 0-.75.75v6h6.75v-6a.75.75 0 0 0-.75-.75Zm2.25 4.81v1.94h1.94l-1.94-1.94Zm3.62-5.918-3.178 3.178a.75.75 0 0 0 0 1.061l3.179 3.179a.75.75 0 0 0 1.06 0l3.18-3.179a.75.75 0 0 0 0-1.06l-3.18-3.18a.75.75 0 0 0-1.06 0Z" />
+                </svg>
+                <span>Projects</span>
+            </li>
+            <li>
+                <span class="marker"></span>
+                <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M15.75 2a2.25 2.25 0 0 1 2.245 2.096L18 4.25v.749L19.75 5a2.25 2.25 0 0 1 2.245 2.096L22 7.25v9.5a2.25 2.25 0 0 1-2.096 2.245L19.75 19H18v.75a2.25 2.25 0 0 1-2.096 2.245L15.75 22h-7.5a2.25 2.25 0 0 1-2.245-2.096L6 19.75V19H4.25a2.25 2.25 0 0 1-2.245-2.096L2 16.75v-9.5a2.25 2.25 0 0 1 2.096-2.245L4.25 5 6 4.999V4.25a2.25 2.25 0 0 1 2.096-2.245L8.25 2h7.5Zm0 1.5h-7.5a.75.75 0 0 0-.743.648L7.5 4.25v15.5c0 .38.282.694.648.743l.102.007h7.5a.75.75 0 0 0 .743-.648l.007-.102V4.25a.75.75 0 0 0-.648-.743L15.75 3.5ZM6 6.499 4.25 6.5a.75.75 0 0 0-.743.648L3.5 7.25v9.5c0 .38.282.694.648.743l.102.007H6V6.499Zm13.75.001L18 6.499V17.5h1.75a.75.75 0 0 0 .743-.648l.007-.102v-9.5a.75.75 0 0 0-.75-.75Z" />
+                </svg>
+                <span>Members</span>
+            </li>
+            <li>
+                <span class="marker"></span>
+                <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M10.125 13.995a2.737 2.737 0 0 0-.617 1.5h-5.26a.749.749 0 0 0-.748.75v.577c0 .536.191 1.054.539 1.461 1.177 1.379 2.984 2.12 5.469 2.205.049.57.273 1.09.617 1.508h-.129c-3.145 0-5.531-.905-7.098-2.739A3.75 3.75 0 0 1 2 16.822v-.578c0-1.19.925-2.164 2.095-2.243l.154-.006h5.876Zm4.621-2.5h3c.648 0 1.18.492 1.244 1.123l.007.127-.001 1.25h1.25c.967 0 1.75.784 1.75 1.75v4.5a1.75 1.75 0 0 1-1.75 1.75h-8a1.75 1.75 0 0 1-1.75-1.75v-4.5c0-.966.784-1.75 1.75-1.75h1.25v-1.25c0-.647.492-1.18 1.123-1.243l.127-.007h3-3Zm5.5 4h-8a.25.25 0 0 0-.25.25v4.5c0 .138.112.25.25.25h8a.25.25 0 0 0 .25-.25v-4.5a.25.25 0 0 0-.25-.25Zm-2.75-2.5h-2.5v1h2.5v-1ZM9.997 2a5 5 0 1 1 0 10 5 5 0 0 1 0-10Zm0 1.5a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7Z" />
+                </svg>
+                <span>Profile</span>
+            </li>
+        </ul>
+    </aside>
+</template>
+<style scoped>
+aside {
+    position: sticky !important;
+    top: 0;
+    background-color: var(--surface-variant-color);
+    height: 100vh;
+}
+
+aside h2 {
+    font-size: var(--big-font);
+    margin: 1.5rem 1rem;
+    color: var(--color-primary-variant);
+}
+
+aside hr {
+    margin: 1rem 0.6rem;
+    border: none;
+    border-top: 1px solid var(--surface-dark-color);
+}
+
+aside ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+
+aside .marker {
+    background-color: var(--color-primary);
+    width: 6px;
+    height: 10%;
+    display: block;
+    position: absolute;
+    top: 50%;
+    left: -8px;
+    translate: 0 -50%;
+    border-top-right-radius: 6px;
+    border-bottom-right-radius: 6px;
+    transition: all 200ms;
+}
+
+aside li {
+    position: relative;
+    padding: 1rem;
+    display: flex;
+    align-items: center;
+    color: var(--color-on-secondary);
+    fill: var(--color-on-secondary);
+    gap: 1rem;
+    font-weight: 600;
+}
+
+
+aside li svg {
+    fill: inherit;
+}
+
+aside li.active {
+    color: var(--color-primary-variant);
+    fill: var(--color-primary-variant);
+    background-color: var(--color-primary-variant-light) !important;
+}
+
+aside li.active .marker {
+    height: 60%;
+    left: 0;
+    background-color: var(--color-primary-variant);
+}
+
+
+aside li:hover {
+    background-color: var(--color-primary-light);
+}
+
+aside li:hover .marker {
+    height: 60%;
+    left: 0;
+}
+</style>
---
+++ front/pages/admin/dashboard.vue
@@ -0,0 +1,162 @@
+<script setup lang='ts'>
+</script>
+<template>
+    <div class="dashboard">
+        <Sidebar />
+
+        <main>
+            <DashboardNav />
+
+            <section v-if="false">
+                <h2>Active Projects</h2>
+                <div class="projects-container">
+
+                    <div class="card" v-for="i in 10">
+                        <div class="header">
+                            <img src="../../public/extra/no_image.png" alt="no_image">
+                            <div>
+                                <h3>Task Planner</h3>
+                                <span>Android</span>
+                            </div>
+                        </div>
+                        <p class="detail">Started: 20 Jun 2023 | Sayam</p>
+                        <p class="description">A simple android app to track and manage your work easily.</p>
+                    </div>
+
+                </div>
+            </section>
+
+            <section>
+                <h2>Projects</h2>
+                <div class="table-holder">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Icon</th>
+                                <th>Name</th>
+                                <th>Manager</th>
+                                <th>Members</th>
+                                <th>Platform</th>
+                                <th>Start</th>
+                                <th>End</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><img src="../../public/extra/no_image.png" alt="no image"></td>
+                                <td>Task Planner</td>
+                                <td>Sayam</td>
+                                <td>5</td>
+                                <td>Android</td>
+                                <td>12 Jan 2023</td>
+                                <td>12 Feb 2023</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+
+        </main>
+    </div>
+</template>
+<style scoped>
+.dashboard {
+    display: grid;
+    grid-template-columns: 300px auto;
+    width: 100%;
+    background-color: var(--surface-color);
+}
+
+
+section {
+    max-width: 1000px;
+    width: 100%;
+    padding: 0 2rem;
+    margin: 3rem auto;
+}
+
+section h2 {
+    color: var(--color-tertiary);
+    font-size: var(--big-font);
+    margin: 1em 0;
+}
+
+section .projects-container {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
+}
+
+.projects-container .card {
+    max-width: 18rem;
+    background-color: white;
+    color: var(--color-on-secondary);
+    padding: 1rem;
+    border-radius: var(--border-radius-medium);
+}
+
+.projects-container .header {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    margin-bottom: 1rem;
+}
+
+.projects-container .header img {
+    width: 60px;
+    border-radius: var(--border-radius-medium);
+}
+
+.projects-container .header h3 {
+    font-size: var(--medium-2-font);
+    font-weight: 600;
+    margin-bottom: 0.4em;
+}
+
+.projects-container .header span {
+    font-size: var(--small-font);
+    background-color: var(--color-primary-variant);
+    padding: 0.4em 1em;
+    border-radius: var(--default-border-radius);
+    color: white;
+}
+
+.projects-container .detail {
+    font-size: var(--small-font);
+    color: var(--color-primary);
+    font-weight: 600;
+    margin: 0.7rem 0;
+}
+
+.projects-container .description {
+    font-size: var(--medium-font);
+    line-height: 1.5;
+}
+
+
+
+
+
+/* ------------------ table ------------------------- */
+
+.dashboard .table-holder{
+    display: block;
+    width: 100%;
+    overflow: auto !important;
+}
+.dashboard table {
+    border-spacing: 1rem;
+    display: block;
+}
+
+.dashboard table img {
+    width: 50px;
+    border-radius: var(--border-radius-medium);
+}
+
+.dashboard table td,
+.dashboard table th {
+    text-align: left;
+    white-space: nowrap;
+}
+</style>
