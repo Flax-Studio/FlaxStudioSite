a0206b999af9eb949c28d98bb9069be146c2407b
nitesh-dev <niteshdev547@gmail.com>
2023-06-09 10:49:35 +0530

    test commit

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,5 +1,8 @@
 <script setup lang='ts'>
 import { releasedAppsData, upcomingAppsData, profileData } from '../data/CommonData';
+const { data } = await useFetch('/api/hello')
+
+console.log(data.value?.hello)
 
 </script>
 <template>
