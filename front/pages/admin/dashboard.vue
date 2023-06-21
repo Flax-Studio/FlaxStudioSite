0aaaa45cb9e6dd3e60838a1604af07d30a5a49d4
nitesh-dev <niteshdev547@gmail.com>
2023-06-21 11:43:24 +0530

    file renamed from product-add to productAdd

--- front/pages/admin/dashboard.vue
+++ front/pages/admin/dashboard.vue
@@ -82,7 +82,7 @@ function markdownToHtml(markdown: string | undefined) {
             <section v-if="activeTabIndex == 0">
                 <h2>Active Projects</h2>
                 <button v-if="dashboardData?.profile.role == 'CEO' || dashboardData?.profile.role == 'CO'">
-                    <NuxtLink to="./product-add">
+                    <NuxtLink to="./productAdd">
                         <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path
                                 d="M11.883 3.007 12 3a1 1 0 0 1 .993.883L13 4v7h7a1 1 0 0 1 .993.883L21 12a1 1 0 0 1-.883.993L20 13h-7v7a1 1 0 0 1-.883.993L12 21a1 1 0 0 1-.993-.883L11 20v-7H4a1 1 0 0 1-.993-.883L3 12a1 1 0 0 1 .883-.993L4 11h7V4a1 1 0 0 1 .883-.993L12 3l-.117.007Z" />
