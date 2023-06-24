0b58ed404ebf03261dabb2abb1488f1b400adffa
nitesh-dev <niteshdev547@gmail.com>
2023-06-24 18:42:22 +0530

    approve public added

--- front/pages/admin/dashboard.vue
+++ front/pages/admin/dashboard.vue
@@ -18,7 +18,7 @@ const isAdmin = ref(false)
 const approvedMembers = ref(Array<AccountDashboardData>())
 const unApprovedMembers = ref(Array<AccountDashboardData>())
 
-let isApproving = false
+let isProcessing = false
 
 
 function changeActiveTab(index: number) {
@@ -32,7 +32,7 @@ onMounted(function () {
 
 async function approveMember(memberId: string, isApproved: boolean, targetButton: EventTarget | null) {
 
-    if (targetButton == null || isApproving == true) return
+    if (targetButton == null || isProcessing == true) return
     const token = localStorage.getItem('token')
 
     if (token == null) {
@@ -44,11 +44,11 @@ async function approveMember(memberId: string, isApproved: boolean, targetButton
     const button = targetButton as HTMLButtonElement
     button.querySelector('div')!!.style.display = 'block'
     button.querySelector('svg')!!.style.display = 'none'
-    isApproving = true
+    isProcessing = true
 
     const res = await Api.approveMember(token!!, isApproved, memberId)
 
-    isApproving = false
+    isProcessing = false
     button.querySelector('div')!!.style.display = 'none'
     button.querySelector('svg')!!.style.display = 'block'
 
@@ -82,17 +82,44 @@ async function approveMember(memberId: string, isApproved: boolean, targetButton
 
 }
 
+async function approvePublic(memberId: string, isPublic: boolean, targetButton: EventTarget | null) {
 
-function approvePublic(memberId: string, isPublic: boolean) {
+    if (targetButton == null || isAdmin.value == false || isProcessing == true) return
     const token = localStorage.getItem('token')
 
     if (token == null) {
-        //throw { message: "You don't have access to server, please signin", statusCode: 401 }
+       
         alert("You don't have access to server, please signin")
         return
     }
-}
 
+    const button = targetButton as HTMLButtonElement
+    button.querySelector('div')!!.style.display = 'block'
+    button.querySelector('svg')!!.style.display = 'none'
+    isProcessing = true
+
+    const res = await Api.approvePublic(token!!, isPublic, memberId)
+
+    isProcessing = false
+    button.querySelector('div')!!.style.display = 'none'
+    button.querySelector('svg')!!.style.display = 'block'
+
+    if (res.isError) {
+        alert(res.error)
+        return
+    } else {
+
+        // on approve success
+        // finding member
+        for (let index = 0; index < dashboardData.value!!.members.length; index++) {
+
+            if (dashboardData.value!!.members[index]._id == memberId) {
+                dashboardData.value!!.members[index].isPublic = isPublic
+                break
+            }
+        }
+    }
+}
 
 async function fetchDataFromServer() {
 
@@ -136,8 +163,6 @@ async function fetchDataFromServer() {
     }
 }
 
-
-
 function countActiveProjects() {
     if (dashboardData.value == undefined) return 0
     let count = 0
@@ -149,8 +174,6 @@ function countActiveProjects() {
 
     return count
 }
-
-
 
 
 function markdownToHtml(markdown: string | undefined) {
@@ -365,7 +388,20 @@ async function deleteProduct(productId: string) {
                                         <span v-else>0</span>
                                     </td>
                                     <td>{{ dateTimeString(member.joinedAt) }}</td>
-                                    <td>{{ member.isPublic }}</td>
+                                    <td>
+                                        <button @click="event => approvePublic(member._id, !member.isPublic, event.target)">
+                                            <div class="loader2"></div>
+                                            <svg v-if="member.isPublic" xmlns="http://www.w3.org/2000/svg" width="24px" viewBox="0 0 24 24" style="fill: var(--color-success);">
+                                                <path
+                                                    d="M12,9A3,3 0 0,1 15,12A3,3 0 0,1 12,15A3,3 0 0,1 9,12A3,3 0 0,1 12,9M12,4.5C17,4.5 21.27,7.61 23,12C21.27,16.39 17,19.5 12,19.5C7,19.5 2.73,16.39 1,12C2.73,7.61 7,4.5 12,4.5M3.18,12C4.83,15.36 8.24,17.5 12,17.5C15.76,17.5 19.17,15.36 20.82,12C19.17,8.64 15.76,6.5 12,6.5C8.24,6.5 4.83,8.64 3.18,12Z" />
+                                            </svg>
+                                            <svg v-else xmlns="http://www.w3.org/2000/svg" width="24px" style="fill: var(--color-error);"
+                                                viewBox="0 0 24 24">
+                                                <path
+                                                    d="M2,5.27L3.28,4L20,20.72L18.73,22L15.65,18.92C14.5,19.3 13.28,19.5 12,19.5C7,19.5 2.73,16.39 1,12C1.69,10.24 2.79,8.69 4.19,7.46L2,5.27M12,9A3,3 0 0,1 15,12C15,12.35 14.94,12.69 14.83,13L11,9.17C11.31,9.06 11.65,9 12,9M12,4.5C17,4.5 21.27,7.61 23,12C22.18,14.08 20.79,15.88 19,17.19L17.58,15.76C18.94,14.82 20.06,13.54 20.82,12C19.17,8.64 15.76,6.5 12,6.5C10.91,6.5 9.84,6.68 8.84,7L7.3,5.47C8.74,4.85 10.33,4.5 12,4.5M3.18,12C4.83,15.36 8.24,17.5 12,17.5C12.69,17.5 13.37,17.43 14,17.29L11.72,15C10.29,14.85 9.15,13.71 9,12.28L5.6,8.87C4.61,9.72 3.78,10.78 3.18,12Z" />
+                                            </svg>
+                                        </button>
+                                    </td>
 
                                     <td>
                                         <NuxtLink target="_blank" :to="'/profile/' + member._id">
@@ -745,6 +781,11 @@ async function deleteProduct(productId: string) {
 
 
 /* ------------------ table ------------------------- */
+
+.dashboard button .loader2 {
+    border-color: var(--color-primary-variant);
+    display: none;
+}
 
 .dashboard button.success .loader2 {
     border-color: var(--color-success);
