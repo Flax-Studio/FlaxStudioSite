0e9e310f6c1ef131caf91e75ecad47a08cd84425
nitesh-dev <niteshdev547@gmail.com>
2024-01-08 22:31:13 +0530

    added tailwindcss with nuxt modules

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+// export default defineNuxtConfig({
+//   devtools: { enabled: true },
+//   modules: ["@nuxtjs/tailwindcss"],
+//   build: {
+//    postcss:{
+
+//    }
+//   },
+// });
+
+export default {
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss"],
+  build: {
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+  },
+};
